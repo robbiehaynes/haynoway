8da15e6f4fbe60b01b3efacf83b24e1d8563d616
Robert Haynes <robhaynes0420@gmail.com>
2024-03-25 18:20:23 +0000

    Added Hero structure and CSS variables start

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,9 @@ module.exports = {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
+        tertiary: {
+          DEFAULT: "hsl(var(--tertiary))"
+        },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
@@ -71,6 +74,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        hero:  'url(/hero/hero-bg.png)'
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
