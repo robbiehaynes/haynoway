0bbc6a665d277187672500b9bb98e94b163ca962
Robert Haynes <robhaynes0420@gmail.com>
2024-03-26 09:23:38 +0000

    CSS variables and hero finished

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -35,9 +35,37 @@ const Hero = () => {
                 Download CV <Download size={18}/>
               </Button>
             </div>
+            {/* socials */}
+            <Socials containerStyles="flex gap-x-6 mx-auto xl:mx-0" iconsStyles="text-foreground text-[22px] hover:text-primary transition-all"/>
           </div>
           {/* image */}
-          <div className="hidden xl:flex relative ">Image</div>
+          <div className="hidden xl:flex relative ">
+            {/* badge 1 */}
+            <Badge 
+              containerStyles="absolute top-[24%] -left-[5rem]"
+              icon={<RiBriefcase4Fill />} 
+              endCountNum={3} 
+              badgeText={"Years Of Experience"} 
+            />
+            {/* badge 2 */}
+            <Badge 
+              containerStyles="absolute top-[80%] -left-[1rem]"
+              icon={<RiTodoFill />} 
+              endCountNum={6} 
+              endCountText={'k'}
+              badgeText={"Finished Projects"} 
+            />
+            {/* badge 3 */}
+            <Badge 
+              containerStyles="absolute top-[55%] -right-8"
+              icon={<RiTeamFill />} 
+              endCountNum={9} 
+              endCountText={'k'}
+              badgeText={"Happy Clients"} 
+            />
+            <div className=" bg-hero_shape_2_light dark:bg-hero_shape_2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom" imgSrc='/hero/developer.png'/>
+          </div>
         </div>
         {/* Icon */}
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
