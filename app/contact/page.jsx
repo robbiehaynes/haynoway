6786c2402ff069b80df6f85e0d11cdadbd9e9566
Robert Haynes <robhaynes0420@gmail.com>
2024-03-28 11:57:43 +0000

    Project and Contact page finished
    Project and Contact page finished. Still need to make contact form functional.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { HomeIcon, PhoneCall, MailIcon } from "lucide-react"
+
+import Form from "@/components/Form"
 
 const Contact = () => {
   return (
-    <div>Contact Page</div>
+    <section>
+      <div className="container mx-auto">
+        {/* text & illustration */}
+        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
+          {/* text */}
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+              <span className="w-[30px] h-[2px] bg-primary"></span>
+              Say Hello 👋
+            </div>
+            <h1 className="h1 max-w-md mb-8">Let's Work Together.</h1>
+            <p className="subtitle max-w-[400px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, ratione autem excepturi eum reprehenderit a minus dignissimos quibusdam facere ab consectetur, quos beatae aspernatur voluptate quia animi quis odio ut.</p>
+          </div>
+          {/* illustration */}
+          <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat">
+
+          </div>
+        </div>
+        {/* contact form */}
+        <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
+          {/* info */}
+          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
+            {/* mail */}
+            <div className="flex items-center gap-x-8">
+              <MailIcon size={18} className="text-primary" />
+              <div>robhaynes0420@gmail.com</div>
+            </div>
+            {/* mail */}
+            <div className="flex items-center gap-x-8">
+              <HomeIcon size={18} className="text-primary" />
+              <div>Manchester, UK</div>
+            </div>
+            {/* mail */}
+            <div className="flex items-center gap-x-8">
+              <PhoneCall size={18} className="text-primary" />
+              <div>+44 77174 62481</div>
+            </div>
+          </div>
+          {/* form */}
+          <Form />
+        </div>
+      </div>
+    </section>
   )
 }
 
