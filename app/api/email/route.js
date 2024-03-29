11ec921d253197b52a7426d760243e9bcd6fdfa8
Robert Haynes <robhaynes0420@gmail.com>
2024-03-29 08:28:15 +0000

    Contact form now functional
    Contact form working using nodemailer and NextJS server API functions.

---
+++ app/api/email/route.js
@@ -0,0 +1,51 @@
+import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
+
+export async function POST(request) {
+  const { email, name, message } = await request.json();
+
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    /* 
+      setting service as 'gmail' is same as providing these setings:
+
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true
+
+      If you want to use a different email provider other than gmail, you need to provide these manually.
+      Or you can go use these well known services and their settings at
+      https://github.com/nodemailer/nodemailer/blob/master/lib/well-known/services.json
+  */
+    auth: {
+      user: process.env.GOOGLE_EMAIL,
+      pass: process.env.GOOGLE_APP_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.GOOGLE_EMAIL,
+    // cc: email, (uncomment this line if you want to send a copy to the sender)
+    subject: `Message from ${name} (${email})`,
+    text: message,
+  };
+
+  const sendMailPromise = () =>
+    new Promise((resolve, reject) => {
+      transport.sendMail(mailOptions, function (err) {
+        if (!err) {
+          resolve('Email sent');
+        } else {
+          reject(err.message);
+        }
+      });
+    });
+
+  try {
+    await sendMailPromise();
+    return NextResponse.json({ message: 'Email sent' });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 500 });
+  }
+}
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,34 +1,84 @@
 'use client';
 
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { User, MailIcon, ArrowRightIcon, MessageSquare } from "lucide-react";
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 const Form = () => {
+
+  const [ errors, setErrors ] = useState([]);
+  const [ isSubmitting, setIsSubmitting ] = useState(false);
+  const { register, handleSubmit } = useForm();
+
+  function sendEmail(data) {
+    const apiEndpoint = '/api/email';
+
+    setIsSubmitting(true);
+  
+    fetch(apiEndpoint, {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        setIsSubmitting(false)
+        setErrors({
+          title: "Success!",
+          description: response.message,
+          variant: ""
+        });
+        setTimeout(() => {
+          setErrors([]);
+        }, 5000); // hide message after 10 seconds
+      })
+      .catch((err) => {
+        setIsSubmitting(false)
+        setErrors({
+          title: "Oh no!",
+          description: response.message,
+          variant: "destructive"
+        });
+        setTimeout(() => {
+          setErrors([]);
+        }, 5000); // hide message after 10 seconds
+      });
+  }
+
   return (
-    <form className="flex flex-col gap-y-4">
+    <form className="flex flex-col gap-y-4" onSubmit={handleSubmit(sendEmail)}>
       {/* input */}
       <div className="relative flex items-center">
-        <Input type='name' id="name" placeholder="Name" />
+        <Input type='text' name="name" id="name" placeholder="Name" {...register('name', { required: true })} />
         <User className="absolute right-6" size={20} />
       </div>
       {/* email */}
       <div className="relative flex items-center">
-        <Input type='email' id="email" placeholder="Email" />
+        <Input type='email' name="email" id="email" placeholder="Email" {...register('email', { required: true })} />
         <MailIcon className="absolute right-6" size={20} />
       </div>
       {/* textarea */}
       <div className="relative flex items-center">
-        <Textarea type='name' id="name" placeholder="Type your message here" />
+        <Textarea type='message' name="message" id="message" placeholder="Type your message here" {...register('message', { required: true })} />
         <MessageSquare className="absolute top-4 right-6" size={20} />
       </div>
-      <Button className="flex items-center gap-x-1 mx-w-[166px]">
-        Let's Talk
+      <Button type="submit" disabled={isSubmitting} className="flex items-center gap-x-1 mx-w-[166px]">
+        Send
         <ArrowRightIcon size={20} />
       </Button>
+      {errors.title && 
+        <Alert className="fixed bottom-10 left-10 p-4 max-w-[300px]" variant={errors.variant}>
+          <AlertTitle>{errors.title}</AlertTitle>
+          <AlertDescription>{errors.description}</AlertDescription>
+        </Alert>
+      }
     </form>
   )
 }
 
-export default Form
+export default Form;
